6fd9897c0f3d28909f52d63e7ee25c3a90323ca7
Nick Makes <nick.makes@mlb.com>
2015-12-22 17:43:25 -0500

    Includes the actual dist/ folder

---
+++ dist/js/mmdb-toolbar.js
@@ -0,0 +1,41 @@
+(function() {
+	'use strict';
+
+	angular.module( 'mmdb.toolbar', [] )
+
+	.provider( 'mmdbToolbar', function() {
+
+		this.setBrand = function(brand) {
+			this.brand = brand;
+		};
+
+		this.setViews = function(views) {
+			this.views = views;
+		};
+
+		this.$get = function() {
+			return this;
+		};
+	} )
+
+	.directive( 'mmdbToolbar', function() {
+		return {
+			restrict : 'E',
+			templateUrl : 'mmdb-toolbar.tmpl.html',
+			controller : 'MmdbToolbarCtrl',
+			controllerAs : 'mmdbToolbar',
+			bindToController : true
+		}
+	} )
+
+	.controller( 'MmdbToolbarCtrl', [ 'mmdbToolbar', MmdbToolbarCtrl ] );
+
+	function MmdbToolbarCtrl(mmdbToolbar) {
+		var vm = this;
+
+		vm.brand = mmdbToolbar.brand;
+		vm.views = mmdbToolbar.views;
+	}
+	
+	angular.module("mmdb.toolbar").run(["$templateCache", function($templateCache) {$templateCache.put("mmdb-toolbar.tmpl.html","<nav class=\"navbar navbar-fixed-top\">\n	<a class=\"navbar-brand\" href=\"#\">{{mmdbToolbar.brand}}</a>\n	<ul class=\"nav navbar-nav navbar-right\">\n		<li ng-repeat=\"view in mmdbToolbar.views\"><a ng-href=\"#\">{{view}}</a></li>\n	</ul>\n</nav>");}]);
+}());
