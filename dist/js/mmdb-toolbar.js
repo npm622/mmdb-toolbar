a213f6fba9132122e8228269faa5a713031ee1f7
Nick Makes <nick.makes@mlb.com>
2015-12-23 12:57:07 -0500

    Adds formatting to tito page

--- dist/js/mmdb-toolbar.js
+++ dist/js/mmdb-toolbar.js
@@ -140,6 +140,6 @@
 	}
 
 	angular.module("mmdb.toolbar").run(["$templateCache", function($templateCache) {$templateCache.put("mmdb-toolbar.tmpl.html","<nav class=\"navbar navbar-fixed-top\">\n	<a class=\"navbar-brand\" ng-href=\"#/\">{{mmdbToolbar.brand}}</a>\n	<ul class=\"nav navbar-nav navbar-right\">\n		<li><a ng-href=\"#/tito\">tito</a></li>\n		<li><a ng-href=\"#/yogi\">yogi</a></li>\n	</ul>\n</nav>");
-$templateCache.put("tito.tmpl.html","<div>\n    <h3>meet tito!</h3>\n    <img ng-src=\"{{tito.imgSrc}}\" class=\"img-rounded\"/>\n</div>\n");
+$templateCache.put("tito.tmpl.html","<div>\n    <h1 class=\"banner-title\">Meet Tito!</h1>\n    <img ng-src=\"{{tito.imgSrc}}\" class=\"center-block img-rounded\"/>\n</div>\n");
 $templateCache.put("yogi.tmpl.html","<div>\n	<h1 class=\"banner-title\">Yogi says...</h1>\n	<blockquote class=\"quote-box\">\n		<p class=\"quote-text\">{{yogi.quote.line}}</p>\n	</blockquote>\n</div>\n");}]);
 }());
