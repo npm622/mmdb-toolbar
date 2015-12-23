759d216613289ddeaee043b9e1e2f216487830ef
Nick Makes <nick.makes@mlb.com>
2015-12-22 22:15:30 -0500

    Attempts to add a tito/ page (ran gulp this time)

--- dist/js/mmdb-toolbar.js
+++ dist/js/mmdb-toolbar.js
@@ -1,7 +1,19 @@
 (function() {
 	'use strict';
 
-	angular.module( 'mmdb.toolbar', [] )
+	angular.module( 'mmdb.toolbar', ['ui.router'] )
+
+	.config( function config($stateProvider) {
+		$stateProvider.state( 'tito', {
+			url : '/tito',
+			templateUrl : 'tito.tmpl.html',
+			controller : 'TitoCtrl',
+			controllerAs : 'tito',
+			data : {
+				pageTitle : 'tito'
+			}
+		} );
+	} )
 
 	.provider( 'mmdbToolbar', function() {
 
@@ -22,13 +34,18 @@
 		return {
 			restrict : 'E',
 			templateUrl : 'mmdb-toolbar.tmpl.html',
+			scope: {
+				homeHref: "@"
+			},
 			controller : 'MmdbToolbarCtrl',
 			controllerAs : 'mmdbToolbar',
 			bindToController : true
 		}
 	} )
 
-	.controller( 'MmdbToolbarCtrl', [ 'mmdbToolbar', MmdbToolbarCtrl ] );
+	.controller( 'MmdbToolbarCtrl', [ 'mmdbToolbar', MmdbToolbarCtrl ] )
+
+	.controller( 'TitoCtrl', [ '$scope', TitoCtrl ] );
 
 	function MmdbToolbarCtrl(mmdbToolbar) {
 		var vm = this;
@@ -36,6 +53,11 @@
 		vm.brand = mmdbToolbar.brand;
 		vm.views = mmdbToolbar.views;
 	}
+
+	function TitoCtrl($scope) {
+		console.log( 'hello tito!' );
+	};
 	
-	angular.module("mmdb.toolbar").run(["$templateCache", function($templateCache) {$templateCache.put("mmdb-toolbar.tmpl.html","<nav class=\"navbar navbar-fixed-top\">\n	<a class=\"navbar-brand\" href=\"#\">{{mmdbToolbar.brand}}</a>\n	<ul class=\"nav navbar-nav navbar-right\">\n		<li ng-repeat=\"view in mmdbToolbar.views\"><a ng-href=\"#\">{{view}}</a></li>\n	</ul>\n</nav>");}]);
+	angular.module("mmdb.toolbar").run(["$templateCache", function($templateCache) {$templateCache.put("mmdb-toolbar.tmpl.html","<nav class=\"navbar navbar-fixed-top\">\n	<a class=\"navbar-brand\" ng-href=\"{{mmdbToolbar.homeHref}}\">{{mmdbToolbar.brand}}</a>\n	<ul class=\"nav navbar-nav navbar-right\">\n		<li ng-repeat=\"view in mmdbToolbar.views\"><a ng-href=\"#\">{{view}}</a></li>\n	</ul>\n</nav>");
+$templateCache.put("tito.tmpl.html","<div>\n    <h3>meet tito!</h3>\n    <img ng-src=\"assets/images/copy-of-tito.jpg\" class=\"img-rounded\"/>\n</div>\n");}]);
 }());
