828901cc5e71f4549f8de45123db7576a0f7ad7f
Nick Makes <nick.makes@mlb.com>
2015-12-22 22:34:03 -0500

    Runs gulp this time

--- dist/js/mmdb-toolbar.js
+++ dist/js/mmdb-toolbar.js
@@ -3,18 +3,6 @@
 
 	angular.module( 'mmdb.toolbar', ['ui.router'] )
 
-	.config( function config($stateProvider) {
-		$stateProvider.state( 'tito', {
-			url : '/tito',
-			templateUrl : 'tito.tmpl.html',
-			controller : 'TitoCtrl',
-			controllerAs : 'tito',
-			data : {
-				pageTitle : 'tito'
-			}
-		} );
-	} )
-
 	.provider( 'mmdbToolbar', function() {
 
 		this.setBrand = function(brand) {
@@ -28,6 +16,26 @@
 		this.$get = function() {
 			return this;
 		};
+	} )
+
+	.config( function config($stateProvider) {
+		$stateProvider.state( 'tito', {
+			url : '/tito',
+			templateUrl : 'tito.tmpl.html',
+			controller : 'TitoCtrl',
+			controllerAs : 'tito',
+			data : {
+				pageTitle : 'tito'
+			}
+		} ).state( 'yogi', {
+			url : '/yogi',
+			templateUrl : 'yogi.tmpl.html',
+			controller : 'YogiCtrl',
+			controllerAs : 'yogi',
+			data : {
+				pageTitle : 'yogi'
+			}
+		} );
 	} )
 
 	.directive( 'mmdbToolbar', function() {
@@ -45,19 +53,25 @@
 
 	.controller( 'MmdbToolbarCtrl', [ 'mmdbToolbar', MmdbToolbarCtrl ] )
 
-	.controller( 'TitoCtrl', [ '$scope', TitoCtrl ] );
+	.controller( 'TitoCtrl', [ '$scope', TitoCtrl ] )
+
+	.controller( 'YogiCtrl', [ '$scope', YogiCtrl ] );
 
 	function MmdbToolbarCtrl(mmdbToolbar) {
 		var vm = this;
 
 		vm.brand = mmdbToolbar.brand;
-		vm.views = mmdbToolbar.views;
 	}
 
 	function TitoCtrl($scope) {
-		console.log( 'hello tito!' );
+		var vm = this;
+	};
+
+	function YogiCtrl($scope) {
+		var vm = this;
 	};
 	
-	angular.module("mmdb.toolbar").run(["$templateCache", function($templateCache) {$templateCache.put("mmdb-toolbar.tmpl.html","<nav class=\"navbar navbar-fixed-top\">\n	<a class=\"navbar-brand\" ng-href=\"{{mmdbToolbar.homeHref}}\">{{mmdbToolbar.brand}}</a>\n	<ul class=\"nav navbar-nav navbar-right\">\n		<li ng-repeat=\"view in mmdbToolbar.views\"><a ng-href=\"#\">{{view}}</a></li>\n	</ul>\n</nav>");
-$templateCache.put("tito.tmpl.html","<div>\n    <h3>meet tito!</h3>\n    <img ng-src=\"assets/images/copy-of-tito.jpg\" class=\"img-rounded\"/>\n</div>\n");}]);
+	angular.module("mmdb.toolbar").run(["$templateCache", function($templateCache) {$templateCache.put("mmdb-toolbar.tmpl.html","<nav class=\"navbar navbar-fixed-top\">\n	<a class=\"navbar-brand\" ng-href=\"#/\">{{mmdbToolbar.brand}}</a>\n	<ul class=\"nav navbar-nav navbar-right\">\n		<li><a ng-href=\"#/tito\">tito</a></li>\n		<li><a ng-href=\"#/yogi\">yogi</a></li>\n	</ul>\n</nav>");
+$templateCache.put("tito.tmpl.html","<div>\n    <h3>meet tito!</h3>\n    <img ng-src=\"assets/images/copy-of-tito.jpg\" class=\"img-rounded\"/>\n</div>\n");
+$templateCache.put("yogi.tmpl.html","<div>\n	<h3>yogi says...</h3>\n	<p>TODO: figure out how to poll for a random yogi berra quote here</p>\n</div>\n");}]);
 }());
